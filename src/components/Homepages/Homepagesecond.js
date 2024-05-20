8ca15218bc5bf2efbb7c654a7a5092cf2bc0800d
Ashish Gautam <ashishgautam9846@gmail.com>
2024-05-20 22:31:16 +0545

    first commit

--- src/components/Homepages/Homepagesecond.js
+++ src/components/Homepages/Homepagesecond.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import '../Homepages/Homepagesecond.css'
 
@@ -27,33 +26,3 @@ export const Homepagesecond = () => {
 
   )
 }
-=======
-import React from 'react'
-import '../Homepages/Homepagesecond.css'
-
-export const Homepagesecond = () => {
-  return (
-    <div className='homepagesecond'>
-
-<div className='video-section'>
-    <div className='enjoydiv'>
-    Enjoy on your TV
-    <figcaption className='des'><span className='description'>Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.</span></figcaption>
-    </div>
-
-
-    <video className='videofirst'autoPlay muted loop>
-        
-        <source src="https://videos.pexels.com/video-files/5082597/5082597-uhd_4096_2160_25fps.mp4" type="video/mp4" />
-
-        </video>
-
-        </div>
-
-    <hr className='hrline'/>
-</div>
-
-
-  )
-}
->>>>>>> 53f3bfd5d1c63a581b40c85deb5410d6f6b5c551
